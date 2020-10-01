3a1985760fc21cf360dbd96fcece628c48e092ed
dmsarmiento <daneahmarelle.sarmiento.iics@ust.edu.ph>
2020-10-01 11:48:54 +0800

    commit2

--- app/src/main/java/com/example/tempo_neah/PomodoroActivity.java
+++ app/src/main/java/com/example/tempo_neah/PomodoroActivity.java
@@ -3,46 +3,12 @@ package com.example.tempo_neah;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.luseen.spacenavigation.SpaceItem;
-import com.luseen.spacenavigation.SpaceNavigationView;
-import com.luseen.spacenavigation.SpaceOnClickListener;
 
 public class PomodoroActivity extends AppCompatActivity {
-
-    SpaceNavigationView bNav; //bot nav bar
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pomodoro);
-
-        //bot nav bar
-        bNav = findViewById(R.id.bottMenu);
-        bNav.initWithSaveInstanceState(savedInstanceState);
-        bNav.showIconOnly();
-        bNav.addSpaceItem(new SpaceItem("calendar", R.drawable.cal));
-        bNav.addSpaceItem(new SpaceItem("notes", R.drawable.notes));
-        bNav.addSpaceItem(new SpaceItem("expenses", R.drawable.exp));
-        bNav.addSpaceItem(new SpaceItem("pomodoro", R.drawable.alarm));
-
-        bNav.setSpaceOnClickListener(new SpaceOnClickListener() {
-            @Override
-            public void onCentreButtonClick() {
-                Toast.makeText(PomodoroActivity.this,"onCentreButtonClick", Toast.LENGTH_SHORT).show();
-                bNav.setCentreButtonSelectable(true);
-            }
-            @Override
-            public void onItemClick(int itemIndex, String itemName) {
-//                Toast.makeText(MainActivity.this, itemIndex + " " + itemName, Toast.LENGTH_SHORT).show();
-
-            }
-            @Override
-            public void onItemReselected(int itemIndex, String itemName) {
-//                Toast.makeText(MainActivity.this, itemIndex + " FUCKING " + itemName, Toast.LENGTH_SHORT).show();
-            }
-        });
-        //bot nav bar
     }
 }
